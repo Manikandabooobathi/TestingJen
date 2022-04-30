43484cdc1b12140fff21cf13fb7579102ef701bb
Manikandan.V <Manikandan.V@8kmiles.local>
2022-04-30 14:02:21 +0530

    1st commit

---
+++ Test/src/Sampleclass.java
@@ -0,0 +1,8 @@
+
+public class Sampleclass {
+	
+	public static void main(String[] args) {
+		System.out.println("This is Test sampe classes");
+	}
+
+}
---
+++ Test/src/login.java
@@ -0,0 +1,12 @@
+
+public class login {
+public static void main(String[] args) {
+	login lg=new login();
+	lg.msin();
+	
+}
+	private void msin() {
+		System.out.println("this is for login test");
+
+	}
+}
