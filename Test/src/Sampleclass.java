3388f1af2dd3d8d5a27efd7cc9109f94a25ba288
MANIKANDAN.V <98746100+Manikandabooobathi@users.noreply.github.com>
2022-05-05 17:43:51 +0530

    Update Sampleclass.java

--- Test/src/Sampleclass.java
+++ Test/src/Sampleclass.java
@@ -3,6 +3,7 @@ public class Sampleclass {
 	
 	public static void main(String[] args) {
 		System.out.println("This is Test sampe classes");
+		System.out.println("This is Test sampe classe2s");
 	}
 
 }
