7cab927fb95b0b6981671ce84426c921b1b93947
MANIKANDAN.V <98746100+Manikandabooobathi@users.noreply.github.com>
2023-01-03 13:18:33 +0530

    wqfa

--- Test/src/Sampleclass.java
+++ Test/src/Sampleclass.java
@@ -2,7 +2,7 @@
 public class Sampleclass {
 	
 	public static void main(String[] args) {
-		System.out.println("This is Test sampe classes");
+		System.out.println("This is Test sassmpe classes");
 		System.out.println("This is Test sampe classe2s");
 	}
 
